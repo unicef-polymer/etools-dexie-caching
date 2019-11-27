c3caba41739c851215d0e59c3b501bfbc80ac9eb
Adriana Trif <trif.corina.adriana@gmail.com>
2019-11-27 19:07:32 +0200

    etools-dexie-caching

---
+++ etools-dexie-caching.d.ts
@@ -0,0 +1,17 @@
+
+type EtoolsEndpoint = {
+  url: string,
+  exp?: number,
+  cacheTableName?: string,
+  cachingKey?: string
+}
+
+declare function requestIsCacheable(method: string, endpoint: EtoolsEndpoint): boolean;
+declare function cacheEndpointResponse(responseData: any, endpoint: EtoolsEndpoint): Promise<any>;
+declare function getFromCache(endpoint: EtoolsEndpoint): Promise<any>;
+
+export {
+  requestIsCacheable,
+  cacheEndpointResponse,
+  getFromCache
+}
